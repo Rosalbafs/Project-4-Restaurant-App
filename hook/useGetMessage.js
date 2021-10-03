cbeff6eb77cf062e893c52c069dcfb2db4e164d8
Rosalba Fuentes <84886793+Rosalbafs@users.noreply.github.com>
2021-10-02 23:21:01 -0500

    Update useGetMessage.js

--- hook/useGetMessage.js
+++ hook/useGetMessage.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const useGetMessage = () => {
   useEffect(() => {
-    axios.get('http://localhost:5000/message')
+    axios.get('http://localhost:3000/message')
       .then((response) => console.log(response.data))
   }, [])
 }
