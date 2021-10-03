d60ccd65df5318176128f0a5a1f6ae0d04ac0d33
Rosalba Fuentes <84886793+Rosalbafs@users.noreply.github.com>
2021-10-02 23:54:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+ const user = {
+    name: "rosalba",
+    email: 'huzinga@gmail.com'
+  };
+  
+  const element = <h1> {user.name} exist email: {user.email} </h1>;
+  
+  ReactDOM.render(
+    element3,
+    
+    document.getElementById('root')
+  );
+  
+  
